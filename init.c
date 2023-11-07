a46caabe4617673770f376fbb4ace990d09f1fbb
Egor Geller <egkingers11@gmail.com>
2023-11-07 23:36:09 +0200

    New

---
+++ init.c
@@ -0,0 +1,59 @@
+#include "defs.h"
+
+int Sq120ToSq64[BRD_SQ_NUM];
+int Sq64ToSq120[64];
+
+U64 setMask[64];
+U64 clearMask[64];
+
+void initBitMasks()
+{
+    for(size_t i = 0; i < 64; i++)
+    {
+        setMask[i] = 0ULL;
+        clearMask[i] = 0ULL;
+    }
+
+    for (size_t i = 0; i < 64; i++)
+    {
+        setMask[i] |= (1ULL << i);
+        clearMask[i] = ~setMask[i];
+    }
+    
+}
+
+void initSq120To64()
+{
+    int index = 0;
+    int file = FILE_A;
+    int rank = RANK_1;
+    int sq = A1;
+    int sq64 = 0; // representing the 64 numbers in the array (board)
+
+    for (index = 0; index < BRD_SQ_NUM; ++index)
+    {
+        Sq120ToSq64[index] = 65; // safety check, value should never be return
+    }
+
+    for (index = 0; index < 64; ++index)
+    {
+        Sq64ToSq120[index] = 120; // safety check, value should never be return
+    }
+
+    for (rank = RANK_1; rank <= RANK_8; ++rank)
+    {
+        for (file = FILE_A; file <= FILE_H; ++file)
+        {
+            sq = FR2SQ(file, rank);
+            Sq64ToSq120[sq64] = sq;
+            Sq120ToSq64[sq] = sq64;
+            sq64++;
+        }
+    }
+}
+
+void allInit()
+{
+    initSq120To64();
+    initBitMasks();
+}
